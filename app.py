837f7befc30ed16bf6969820f23c6ba22051e1d6
Vishal Karri <vishal.reddykarri@bbc.co.uk>
2022-11-08 12:45:30 +0000

    Co-authored-by: aaronx21 <aaronmrdj21@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route('/')
-
-def index():
-    return "Hello, world!"
+def home():
+   return render_template('Home.html')
+if __name__ == '__main__':
+   app.run()
