3493aaae02c6a8bdecc0e9347e1d050cba7af8cd
“JoshuaCaddy” <joshuagcaddy@gmail.com>
2022-11-08 15:56:26 +0000

    Added functionality

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+import jyserver.Flask as jsf
 
-@app.route('/')
+app = Flask(__name__)
+@jsf.use(app)
+class App:
+   def __init__(self) -> None:
+      pass
+   def test(self):
+      self.js.document.getElementById("hello").innerText="hello mate"
+@app.route("/")
 def home():
-   return render_template('Home.html')
-if __name__ == '__main__':
+   return App.render(render_template("Home.html"))
+if __name__ == "__main__":
    app.run()
+
