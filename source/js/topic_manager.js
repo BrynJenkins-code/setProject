7d70e0bdf75e4a603f9f4a838303d39941b9afde
Vishal Karri <vishal.reddykarri@bbc.co.uk>
2022-11-09 09:31:01 +0000

    Clean up

--- source/js/topic_manager.js
+++ source/js/topic_manager.js
@@ -1,26 +1,23 @@
 function loadTopics(jsonData) {
-    const topics = []
+  const topics = [];
 
-    for (i in jsonData.quiz) {
-        topics.push(jsonData.quiz[i].topic)
-    }
-    return topics;
+  for (i in jsonData.quiz) {
+    topics.push(jsonData.quiz[i].topic);
+  }
+  return topics;
 }
 
-topics = loadTopics(questions)
-console.log(topics);
+topics = loadTopics(questions);
 createLinks(topics);
 
-
 function createLinks(topics) {
-    console.log(topics.length);
-    for (i = 0; i < topics.length; i++) {
-        console.log("test");
-        var a = document.createElement('a');
-        var linkText = document.createTextNode(topics[i]);
-        a.appendChild(linkText);
-        a.title = topics[i];
-        a.href = "quiz.html";
-        topiclinks.appendChild(a);
-    }
+  console.log(topics.length);
+  for (i = 0; i < topics.length; i++) {
+    var a = document.createElement("a");
+    var linkText = document.createTextNode(topics[i]);
+    a.appendChild(linkText);
+    a.title = topics[i];
+    a.href = "quiz.html";
+    topiclinks.appendChild(a);
+  }
 }
